6383ee8a715b078540908ac1f8ed366335e7524e
Bushra <bushrajafri21196@gmail.com>
2024-07-30 13:38:13 +0530

    do all correction in website check tags and white space change home page banner image

--- assets/js/script.js
+++ assets/js/script.js
@@ -226,3 +226,16 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+// header background change
+
+
+const navbar = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
