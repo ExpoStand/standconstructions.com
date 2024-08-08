c3207d939a271618e5c1e89e0755c8235bc506c7
Bushra <bushrajafri21196@gmail.com>
2024-08-08 16:16:50 +0530

    done changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -243,25 +243,18 @@ window.addEventListener('scroll', () => {
     }
 });
 
-
-
-//Get all the hyperlink elements
-var links = document.getElementsByTagName("a");
-
-//Browse the previously created array
-Array.prototype.forEach.call(links, function(elem, index) {
-  //Get the hyperlink target and if it refers to an id go inside condition
-  var elemAttr = elem.getAttribute("href");
-  if(elemAttr && elemAttr.includes("#")) {
-    //Replace the regular action with a scrolling to target on click
-    elem.addEventListener("click", function(ev) {
-      ev.preventDefault();
-      //Scroll to the target element using replace() and regex to find the href's target id
-      document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-          inline: "nearest"
-          });
+document.querySelectorAll("a[href^='#']").forEach(function(anchor) {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      var targetId = this.getAttribute('href');
+      var elem = document.querySelector(targetId);
+      
+      /* check for broken link */
+      if (elem) {
+        window.scrollTo({
+          top: elem.offsetTop,
+          behavior: 'smooth'
+        });
+      }
     });
-  }
-});
+  });
